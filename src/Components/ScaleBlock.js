92f92f98f47b65682c8ff5c344b832cac941b7c3
Paraskevas Stamatiadis <stamatiadis@telecom-paris.fr>
2026-04-23 17:52:48 +0200

    Update scale6

--- src/Components/ScaleBlock.js
+++ src/Components/ScaleBlock.js
@@ -2,12 +2,11 @@ import { Select, Option } from "@material-tailwind/react";
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 
-
 function valuetext(value) {
     return `${value}`;
 }
 
-export default function ScaleBlock({question, scale: scaleProp, handleOptionSelect, audioIndex, questionIndex, selectedOptions}) {
+export default function ScaleBlock({ question, scale: scaleProp, handleOptionSelect, audioIndex, questionIndex, selectedOptions }) {
     const scale = scaleProp || question.scale;
 
     if (scale.type === 'discrete') {
@@ -15,7 +14,8 @@ export default function ScaleBlock({question, scale: scaleProp, handleOptionSele
             <div className="flex flex-row flex-wrap justify-center gap-4">
                 {scale.labels.map((option, optionIndex) => (
                     <label key={audioIndex + '_' + optionIndex} className="flex items-center cursor-pointer">
-                        <input className='mr-1'
+                        <input
+                            className="mr-1"
                             type="radio"
                             name={`question-${audioIndex + '_' + questionIndex}`}
                             checked={selectedOptions[audioIndex] === option}
@@ -25,8 +25,9 @@ export default function ScaleBlock({question, scale: scaleProp, handleOptionSele
                     </label>
                 ))}
             </div>
-        )
+        );
     }
+
     if (scale.type === 'dropdown') {
         return (
             <div className="w-[200px]">
@@ -34,7 +35,7 @@ export default function ScaleBlock({question, scale: scaleProp, handleOptionSele
                     label="Select"
                     value={selectedOptions[audioIndex]}
                     onChange={(e) => handleOptionSelect(e, audioIndex)}
-                    size='md'
+                    size="md"
                     className="text-left items-start content-start"
                 >
                     {scale.labels.map((option, optionIndex) => (
@@ -44,33 +45,30 @@ export default function ScaleBlock({question, scale: scaleProp, handleOptionSele
                     ))}
                 </Select>
             </div>
-        )
+        );
     }
+
     if (scale.type === 'continuous') {
         const marks = scale.labels.map((label, index) => ({
             value: scale.values[index],
-            label: label
+            label: label,
         }));
 
         return (
-            <Box sx={{ width: '100%', maxWidth: 700, margin: '0 auto', px: 2, pb: 6 }}>
+            <Box sx={{ width: '100%', maxWidth: 700, margin: '0 auto', px: 3, pb: 6 }}>
                 <Slider
                     aria-label="Rating"
                     defaultValue={(scale.range[0] + scale.range[1]) / 2}
+                    min={scale.range[0]}
+                    max={scale.range[1]}
                     getAriaValueText={valuetext}
                     valueLabelDisplay="auto"
                     marks={marks}
-                    onChange={(e) => handleOptionSelect(e.target.value, audioIndex)}
-                    sx={{
-                      '&.MuiSlider-root': {
-                        paddingLeft: 0,
-                        paddingRight: 0
-                      },
-                    }}
+                    onChange={(e, value) => handleOptionSelect(value, audioIndex)}
                 />
             </Box>
-        )
+        );
     }
 
-    return <p>Error! scale type not supported</p>
+    return <p>Error! scale type not supported</p>;
 }
