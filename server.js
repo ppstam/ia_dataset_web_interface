945747f8a13f66dcde8bc22b2e5188e36817e0c1
Paraskevas Stamatiadis <stamatiadis@telecom-paris.fr>
2026-04-24 16:43:10 +0200

    update server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Uncomment to run locally
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
